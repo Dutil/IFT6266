18560cf3914e17517dd90517b024565ba820406d
Francis Dutil <frdutil@gmail.com>
2017-04-21 16:04:39 -0400

    Adding the skip connections and start of the CapGan

--- GAN.py
+++ GAN.py
@@ -6,7 +6,7 @@ import pickle as pkl
 import utils
 import numpy as np
 import models
-
+import os
 
 class GAN(models.Model):
 
@@ -16,39 +16,44 @@ class GAN(models.Model):
         self.last_loss = [0,0]
 
     def initialise(self):
-        #TODO: build generator, discriminator
         generator_fn, discriminator_fn = self.build_network()
 
         self.generator = generator_fn()
         self.discriminator = discriminator_fn()
 
-    def reload(self, model_file):
+    def reload(self, model_dir):
         
         print "reloading..."
+        model_file = os.path.join(model_dir, 'model')
 
         self.options = pkl.load(open(model_file + '.pkl'))
         self.initialise()
         
         # Reloading the generator
-        with np.load(model_file + "_generator.npy.npz") as f:
+        with np.load(model_file + "_generator.npz") as f:
             param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+            print [x.shape for x in param_values]
             lasagne.layers.set_all_param_values(self.generator, param_values)
 
         # Reloading th ediscriminator
-        with np.load(model_file + "_discriminator.npy.npz") as f:
+        with np.load(model_file + "_discriminator.npz") as f:
             param_values = [f['arr_%d' % i] for i in range(len(f.files))]
             lasagne.layers.set_all_param_values(self.discriminator, param_values)
 
 
-    def save(self, model_file):
-        np.savez(model_file+"_generator.npy", *lasagne.layers.get_all_param_values(self.generator))
-        np.savez(model_file+"_discriminator.npy", *lasagne.layers.get_all_param_values(self.discriminator))
+    def save(self, model_dir):
+
+        model_file = os.path.join(model_dir, 'model')
+
+        print "len:", [x.shape for x in lasagne.layers.get_all_param_values(self.generator)]
+
+        np.savez(model_file+"_generator.npz", *lasagne.layers.get_all_param_values(self.generator))
+        np.savez(model_file+"_discriminator.npz", *lasagne.layers.get_all_param_values(self.discriminator))
         option_file = model_file + '.pkl'
         pkl.dump(self.options, open(option_file, 'w'))
 
     def build_generator(self, input_var=None):
         num_units = self.options['generator_num_units']
-        encoder_size = self.options['generator_encoder_size']
         noise_size = self.options['noise_size']
 
         print "We have {} hidden units".format(num_units)
@@ -76,7 +81,6 @@ class GAN(models.Model):
 
     def build_discriminator(self, input_var=None):
         num_units = self.options['discriminator_num_units']
-        encoder_size = self.options['discriminator_encoder_size']
         lrelu = lasagne.nonlinearities.LeakyRectify(0.2)
 
         print "We have {} hidden units".format(num_units)
@@ -107,7 +111,8 @@ class GAN(models.Model):
         #WGAN loss
         use_wgan = self.options['use_wgan']
         l2_penalty = self.options['l2_penalty']
-        
+        gan_penalty = self.options['gan_penalty']
+
         if use_wgan:
             print "Using the wgan loss"
             discriminator_loss=-0.5*((disc_real-disc_sample).mean())
@@ -117,13 +122,13 @@ class GAN(models.Model):
             gan_generator_loss = -T.log(disc_sample).mean()
 
         l2_loss = (l2_penalty)*lasagne.objectives.squared_error(sample, real_img).mean()
-        generator_loss = gan_generator_loss + l2_loss
-        return discriminator_loss, generator_loss, [gan_generator_loss, l2_loss]
+        generator_loss = gan_penalty*gan_generator_loss + l2_loss
+        return discriminator_loss, generator_loss, [gan_penalty*gan_generator_loss, l2_loss]
     
     def _get_sample(self, input_vars):
         return lasagne.layers.get_output(self.generator, *input_vars)
     
-    def _get_disc_score(self, sample, inputs_var):
+    def _get_disc_score(self, sample, inputs_var, real=True):
         return lasagne.layers.get_output(self.discriminator, sample)
 
     def compile_theano_func(self, lr):
@@ -137,8 +142,8 @@ class GAN(models.Model):
         sample = self._get_sample(input_vars)
 
         #Discriminator
-        disc_real = self._get_disc_score(real_img, input_vars)
-        disc_sample = self._get_disc_score(sample, input_vars)
+        disc_real = self._get_disc_score(real_img, input_vars, real=True)
+        disc_sample = self._get_disc_score(sample, input_vars, real=False)
 
         # Our loss
         discriminator_loss, generator_loss, debug_values = self.get_loss(disc_real, disc_sample, real_img, sample)
@@ -299,7 +304,6 @@ class BICGAN(GAN):
     
     def build_generator(self, contour=None, noise=None):
         num_units = self.options['generator_num_units']
-        encoder_size = self.options['generator_encoder_size']
         noise_size = self.options['noise_size']
 
         print "We have {} hidden units".format(num_units)
@@ -389,7 +393,7 @@ class BICGAN(GAN):
         return lasagne.layers.get_output(self.generator,
                                              {self._noise_input:input_vars[0], self._contour_input:input_vars[1]})
     
-    def _get_disc_score(self, sample, inputs_var):
+    def _get_disc_score(self, sample, inputs_var, real=True):
         
         # We set the middle
         contour = inputs_var[1] 
@@ -444,39 +448,222 @@ class BICGAN(GAN):
         return val_fn
     
 class ConditionalGAN(BICGAN):
-
-    def build_discriminator(self, input_var=None):
-        num_units = self.options['discriminator_num_units']
-        encoder_size = self.options['discriminator_encoder_size']
-        lrelu = lasagne.nonlinearities.LeakyRectify(0.2)
+    def build_generator(self, contour=None, noise=None):
+        num_units = self.options['generator_num_units']
+        encoder_size = self.options['generator_encoder_size']
+        noise_size = self.options['noise_size']
 
         print "We have {} hidden units".format(num_units)
 
-        network = lasagne.layers.InputLayer(shape=(None, 3, 32, 32),
-                                            input_var=input_var)
+        # Our encoder
+        self._contour_input = lasagne.layers.InputLayer(shape=(None, 3, 64, 64),
+                                                        input_var=contour)
 
-        # We have one aditionnal layer.
-        network = lasagne.layers.Conv2DLayer(network, num_filters=num_units / 8, filter_size=(5, 5),
-                                             stride=2, pad=2, nonlinearity=lrelu)
-        network = lasagne.layers.BatchNormLayer(network)
+        # encoder = lasagne.layers.BatchNormLayer(self._contour_input)
+        encoder = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(self._contour_input, num_filters=num_units / 8, filter_size=(5, 5),
+                                       stride=2, pad=2))
 
-        network = lasagne.layers.Conv2DLayer(network, num_filters=num_units / 4, filter_size=(5, 5),
-                                             stride=2, pad=2, nonlinearity=lrelu)
-        network = lasagne.layers.BatchNormLayer(network)
+        encoder = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder, num_filters=num_units / 4, filter_size=(5, 5),
+                                       stride=2, pad=2))
 
-        network = lasagne.layers.Conv2DLayer(network, num_filters=num_units / 2, filter_size=(5, 5),
-                                             stride=2, pad=2, nonlinearity=lrelu)
-        network = lasagne.layers.BatchNormLayer(network)
+        encoder = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder, num_filters=num_units / 2, filter_size=(5, 5),
+                                       stride=2, pad=2))
 
-        network = lasagne.layers.Conv2DLayer(network, num_filters=num_units, filter_size=(5, 5),
-                                             stride=2, pad=2, nonlinearity=lrelu)
-        network = lasagne.layers.BatchNormLayer(network)
+        encoder = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder, num_filters=num_units, filter_size=(5, 5),
+                                       stride=2, pad=2))
 
-        network = lasagne.layers.FlattenLayer(network)
-        network = lasagne.layers.DenseLayer(network, 1,
-                                            nonlinearity=lasagne.nonlinearities.sigmoid)
+        encoder = lasagne.layers.FlattenLayer(encoder)
+
+        self._noise_input = lasagne.layers.InputLayer(shape=(None, noise_size),
+                                                      input_var=noise)
+
+        # Merging the encoder and the noise.
+        network = lasagne.layers.ConcatLayer([self._noise_input, encoder])
+
+        network = lasagne.layers.ReshapeLayer(network, (-1, noise_size + num_units * 4 * 4, 1, 1))
+
+        network = lasagne.layers.batch_norm(
+            lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units, filter_size=(4, 4),
+                                                 stride=(1, 1)))
+
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 2,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=8))
+
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 4,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=16))
+
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 8,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=32))
+
+        network = lasagne.layers.TransposedConv2DLayer(network, num_filters=3,
+                                                       filter_size=(5, 5), stride=(2, 2), crop=2,
+                                                       nonlinearity=lasagne.nonlinearities.sigmoid,
+                                                       output_size=64)
 
         return network
 
-    def _get_disc_score(self, sample, inputs_var):
-        return lasagne.layers.get_output(self.discriminator, sample)
+    def _get_sample(self, input_vars):
+        return lasagne.layers.get_output(self.generator,
+                                         {self._noise_input: input_vars[0], self._contour_input: input_vars[1]})
+
+    def _get_disc_score(self, sample, inputs_var, real=True):
+
+        if real:
+            # We set the middle
+            contour = inputs_var[1]
+            contour = utils.put_in_middle_theano(contour, sample)
+
+            return lasagne.layers.get_output(self.discriminator, contour)
+
+        else:
+            # For this model, it is already 64x64
+            return lasagne.layers.get_output(self.discriminator, sample)
+
+    def get_loss(self, disc_real, disc_sample, real_img, sample):
+
+        # WGAN loss
+        use_wgan = self.options['use_wgan']
+        l2_penalty = self.options['l2_penalty']
+        gan_penalty = self.options['gan_penalty']
+
+        if use_wgan:
+            print "Using the wgan loss"
+            discriminator_loss = -0.5 * ((disc_real - disc_sample).mean())
+            gan_generator_loss = -0.5 * (disc_sample.mean())
+        else:
+            discriminator_loss = -(T.log(disc_real) + T.log(1. - disc_sample)).mean()
+            gan_generator_loss = -T.log(disc_sample).mean()
+
+        # The L2 loss
+        center = (sample.shape[2] / 2, sample.shape[3] / 2)
+        center_sample = sample[:, :, center[0] - 16:center[0] + 16, center[1] - 16:center[1] + 16]
+        center_img = real_img
+        l2_loss = (l2_penalty) * lasagne.objectives.squared_error(center_sample, center_img).mean()
+        generator_loss = gan_penalty*gan_generator_loss + l2_loss
+        return discriminator_loss, generator_loss, [gan_penalty*gan_generator_loss, l2_loss]
+
+class SkipConditionalGAN(ConditionalGAN):
+
+
+    def build_generator(self, contour=None, noise=None):
+        num_units = self.options['generator_num_units']
+        noise_size = self.options['noise_size']
+
+        print "We have {} hidden units".format(num_units)
+
+        # Our encoder
+        self._contour_input = lasagne.layers.InputLayer(shape=(None, 3, 64, 64),
+                                                        input_var=contour)
+
+        # encoder = lasagne.layers.BatchNormLayer(self._contour_input)
+        encoder_l1 = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(self._contour_input, num_filters=num_units / 8, filter_size=(5, 5),
+                                       stride=2, pad=2))
+
+        encoder_l2 = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder_l1, num_filters=num_units / 4, filter_size=(5, 5),
+                                       stride=2, pad=2))
+
+        encoder_l3 = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder_l2, num_filters=num_units / 2, filter_size=(5, 5),
+                                       stride=2, pad=2))
+
+        encoder_l4 = lasagne.layers.batch_norm(
+            lasagne.layers.Conv2DLayer(encoder_l3, num_filters=num_units, filter_size=(5, 5),
+                                       stride=2, pad=2))
+
+        encoder = lasagne.layers.FlattenLayer(encoder_l4)
+
+        self._noise_input = lasagne.layers.InputLayer(shape=(None, noise_size),
+                                                      input_var=noise)
+
+        # Merging the encoder and the noise.
+        network = lasagne.layers.ConcatLayer([self._noise_input, encoder])
+
+        network = lasagne.layers.ReshapeLayer(network, (-1, noise_size + num_units * 4 * 4, 1, 1))
+
+        network = lasagne.layers.batch_norm(
+            lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units, filter_size=(4, 4),
+                                                 stride=(1, 1)))
+        
+        network = lasagne.layers.ConcatLayer([network, encoder_l4])
+
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 2,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=8))
+
+        network = lasagne.layers.ConcatLayer([network, encoder_l3])
+        
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 4,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=16))
+
+        network = lasagne.layers.ConcatLayer([network, encoder_l2])
+        
+        network = lasagne.layers.batch_norm(lasagne.layers.TransposedConv2DLayer(network, num_filters=num_units / 8,
+                                                                                 filter_size=(5, 5), stride=(2, 2),
+                                                                                 crop=2, output_size=32))
+
+
+        #network = lasagne.layers.ConcatLayer([network, encoder_l1])
+
+        network = lasagne.layers.TransposedConv2DLayer(network, num_filters=3,
+                                                       filter_size=(5, 5), stride=(2, 2), crop=2,
+                                                       nonlinearity=lasagne.nonlinearities.sigmoid,
+                                                       output_size=64)
+
+        return network
+
+class CapGAN(GAN):
+
+    def __init__(self, options, it = None):
+        super(CapGAN, self).__init__(options, it)
+
+        self.caps_model = models.caps_model(self.options, self.it)
+
+    def build_generator(self, input_var=None):
+
+        self.caps_model.options = self.options
+        self.caps_model.it = self.it
+        network_fn = self.caps_model.build_network()
+        return network_fn()
+
+    def _get_inputs(self):
+        return self.caps_model._get_inputs()
+
+    def train(self, imgs, target, caps):
+
+
+        caps = [cap[np.random.choice(len(cap))] for cap in caps]
+        caps = text_utils.pad_to_the_max(caps)
+
+
+        [disc_loss] = self.train_discriminator_fn(caps, target)
+        gen_loss = self.train_generator_fn(caps, target)
+
+        use_wgan = self.options['use_wgan']
+        if use_wgan:
+            discriminator_params_values = lasagne.layers.get_all_param_values(self.discriminator, trainable=True)
+            clamped_weights = [np.clip(w, -0.05, 0.05) for w in discriminator_params_values]
+            lasagne.layers.set_all_param_values(self.discriminator, clamped_weights, trainable=True)
+
+        return disc_loss, gen_loss
+
+    def get_generation_fn(self):
+        noise_size = self.options['noise_size']
+
+        def val_fn(imgs, target, caps):
+            caps = [cap[np.random.choice(len(cap))] for cap in caps]
+            caps = text_utils.pad_to_the_max(caps)
+
+            res = self.generate_sample_fn(caps)
+            return 0, res[0]
+
+        return val_fn
